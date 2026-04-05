8218e164b0dac7b8b03f781de219e20233163f22
Patience Hellen <patiencehellen91@gmail.com>
2026-04-05 14:24:52 +0300

    Settings.py update

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPES = (
+        ('patient', 'Patient'),
+        ('provider', 'Provider'),
+    )
+
+    user_type = models.CharField(max_length=10, choices=USER_TYPES)
