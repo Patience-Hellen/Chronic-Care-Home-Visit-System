f670081f6ba73f36537e8f49185a7cd3f0756838
Banzy Nyagah <wamuyuhbanzy@gmail.com>
2026-04-09 13:39:58 +0300

    Update admin.py

--- backend/alerts/admin.py
+++ backend/alerts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient, HealthReading, Alert, HomeVisit, VisitNote
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(HealthReading)
+admin.site.register(Alert)
+admin.site.register(HomeVisit)
+admin.site.register(VisitNote)
